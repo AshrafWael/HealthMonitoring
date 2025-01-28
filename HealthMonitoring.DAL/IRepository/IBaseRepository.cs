58ae1b0aa839d9a4ad6340ad1882fbcebf155715
Ashraf Wael <ashrafwael582@gmail.com>
2025-01-28 14:05:24 +0200

    add Base Generic Repository

---
+++ HealthMonitoring.DAL/IRepository/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using HealthMonitoring.DAL.Consts;
+
+namespace HealthMonitoring.DAL.IRepository
+{
+    public interface IBaseRepository<T> where T : class
+    {
+         Task<IEnumerable<T>> GetAllAsync();
+         Task<T> GetByIdAsync(int id);
+         Task<T> FindAsync(Expression<Func<T, bool>> Match, string[] includs = null);
+         Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> Match,int? take,int? skip, string[] includs = null,
+                                    Expression<Func<T,Object>> ordereby= null,string orderbydirection = OrderBy.Ascending  );
+
+    }
+}
