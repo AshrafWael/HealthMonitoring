4e491322fb7010f998a6adb3b5f08664144173d1
Ashraf Wael <ashrafwael582@gmail.com>
2025-01-28 11:48:31 +0200

    Add ApplicationDb Contexxt And Dbsets

--- HealthMonitoring.DAL/Data/DbHelper/HealthMonitoringContext.cs
+++ HealthMonitoring.DAL/Data/DbHelper/HealthMonitoringContext.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HealthMonitoring.DAL.Configrations;
+using HealthMonitoring.DAL.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HealthMonitoring.DAL.Data.DbHelper
 {
-    public class HealthMonitoringContext :DbContext
+    public class HealthMonitoringContext :IdentityDbContext<ApplicationUser>
     {
 
-        public HealthMonitoringContext()
-        {
-            
-        }
+        public HealthMonitoringContext(DbContextOptions<HealthMonitoringContext> dbContext) : base(dbContext) { }
 
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<ActivityData> ActivityDatas { get; set; }
+        public DbSet<EmergencyContacts> EmergencyContacts { get; set; }
+        public DbSet<HealthSuggestions> HealthSuggestions { get; set; }
+        public DbSet<HeartRateData> HeartRateDatas { get; set; }
+        public DbSet<HealthInformation> HealthInformations { get; set; }
+        public DbSet<MedicalNews> MedicalNewsDatas { get; set; }
+        public DbSet<Notification> Notifications { get; set; }  
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new ApplicationUserEntityTypeConfigration());
+            modelBuilder.ApplyConfiguration(new ActivityDataEntityTypeConfigration());
+            modelBuilder.ApplyConfiguration(new EmergencyContactEntityTypeConfigration());
+            modelBuilder.ApplyConfiguration(new HealthInformationEntityTypeConfigration());
+            modelBuilder.ApplyConfiguration(new HealthSuggestionsEntityTypeConfigration());
+            modelBuilder.ApplyConfiguration(new HeartRateDataEntityTypeConfigration());
+            modelBuilder.ApplyConfiguration(new MedicalNewsEntityTypeConfigration());
+            modelBuilder.ApplyConfiguration(new NotificationsEntityTypeConfigration());
             base.OnModelCreating(modelBuilder);
         }
 
