bc594039101a352268d6e8f5df8fad1397910426
Ashraf Wael <ashrafwael582@gmail.com>
2025-01-29 08:51:30 +0200

    Add Unit of Work to Repository

---
+++ HealthMonitoring.DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthMonitoring.DAL.Data.Models;
+using HealthMonitoring.DAL.IRepository;
+
+namespace HealthMonitoring.DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //Refrance for all repos 
+        IActivityDataRepository ActivityDatas { get; }
+        IBaseRepository<EmergencyContact> EmergencyContacts { get; }
+        int SaveChanges ();
+    }
+}
---
+++ HealthMonitoring.DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthMonitoring.DAL.Data.DbHelper;
+using HealthMonitoring.DAL.Data.Models;
+using HealthMonitoring.DAL.IRepository;
+using HealthMonitoring.DAL.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthMonitoring.DAL.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+       private readonly HealthMonitoringContext _dbcontext;
+       public  IActivityDataRepository  ActivityDatas { get; private set; }
+       public IBaseRepository<EmergencyContact> EmergencyContacts { get; private set; }
+        public UnitOfWork(HealthMonitoringContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+            ActivityDatas = new ActivityDataRepository(_dbcontext);
+            EmergencyContacts = new BaseRepository<EmergencyContact>(_dbcontext);
+        }
+        public int SaveChanges()
+        { 
+          return _dbcontext.SaveChanges();
+        }    
+        public void Dispose()
+        {
+           _dbcontext.Dispose();
+        }
+
+    }
+}
