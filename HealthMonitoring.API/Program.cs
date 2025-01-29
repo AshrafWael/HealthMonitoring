6dc493f56d8813e7b10d48581dba26cd031a70a4
Ashraf Wael <ashrafwael582@gmail.com>
2025-01-29 08:48:45 +0200

    add unit of work registration

--- HealthMonitoring.API/Program.cs
+++ HealthMonitoring.API/Program.cs
@@ -1,6 +1,9 @@
 
 using HealthMonitoring.DAL.Data.DbHelper;
 using HealthMonitoring.DAL.Data.Models;
+using HealthMonitoring.DAL.IRepository;
+using HealthMonitoring.DAL.Repository;
+using HealthMonitoring.DAL.UnitOfWork;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +20,10 @@ builder.Services.AddDbContext<HealthMonitoringContext>(options =>
 });
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<HealthMonitoringContext>();
+
+//builder.Services.AddScoped(typeof(IBaseRepository<>),typeof(BaseRepository<>));
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
