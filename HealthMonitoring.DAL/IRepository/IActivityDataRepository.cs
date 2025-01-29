7a0ece48923798fadd9f897dfed5b462f6f9e60e
Ashraf Wael <ashrafwael582@gmail.com>
2025-01-29 08:50:36 +0200

    add interface for spesfic Activetydata repo

---
+++ HealthMonitoring.DAL/IRepository/IActivityDataRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthMonitoring.DAL.Data.Models;
+
+namespace HealthMonitoring.DAL.IRepository
+{
+    public interface IActivityDataRepository :IBaseRepository<ActivityData>
+    {
+
+    }
+}
