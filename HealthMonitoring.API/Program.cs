5d2790899080dfbba7c4bd0c37a2f52b7f82b02b
Ashraf Wael <ashrafwael582@gmail.com>
2025-02-27 13:29:39 +0200

    feat :add conection string and Services Registeration

--- HealthMonitoring.API/Program.cs
+++ HealthMonitoring.API/Program.cs
@@ -1,11 +1,17 @@
 
+using System.Text;
+using HealthMonitoring.BLL.AutoMapper;
+using HealthMonitoring.BLL.IServices;
+using HealthMonitoring.BLL.Services;
 using HealthMonitoring.DAL.Data.DbHelper;
 using HealthMonitoring.DAL.Data.Models;
 using HealthMonitoring.DAL.IRepository;
 using HealthMonitoring.DAL.Repository;
 using HealthMonitoring.DAL.UnitOfWork;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,14 +22,41 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<HealthMonitoringContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Cs"));
+    options.UseSqlServer( builder.Configuration.GetConnectionString("Cs"));
+   
 });
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<HealthMonitoringContext>();
+builder.Services.AddAutoMapper(typeof(MppingProfille));
+
 
 //builder.Services.AddScoped(typeof(IBaseRepository<>),typeof(BaseRepository<>));
 builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+builder.Services.AddScoped<IActivityDataServices, ActivityDataServices>();
+builder.Services.AddScoped<IAuthServices, AuthServices>();
+#region Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<HealthMonitoringContext>();
+#endregion
+#region Authenyication
+var key = builder.Configuration.GetValue<string>("ApiSettings:SecretKey");
+builder.Services.AddAuthentication(a =>
+{
+    a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
 
+
+});
+#endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
