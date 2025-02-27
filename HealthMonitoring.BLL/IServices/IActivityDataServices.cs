cc7629c6024167261b69588e528d6ce133cded09
Ashraf Wael <ashrafwael582@gmail.com>
2025-02-27 13:30:32 +0200

    feat:add services Interface

---
+++ HealthMonitoring.BLL/IServices/IActivityDataServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthMonitoring.BLL.Dtos.ActivityDataDtos;
+
+namespace HealthMonitoring.BLL.IServices
+{
+    public interface IActivityDataServices
+    {
+       Task< IEnumerable<ActivityDataReadDto>> GetAll();
+        Task<ActivityDataReadDto> GetById(int id);
+        Task  Add(ActivityDataCreateDto activitycreatdto);
+        Task Update(ActivityDataUpdateDto activityupdatedto);
+        Task Delete(int Id);
+       
+    }
+}
---
+++ HealthMonitoring.BLL/IServices/IAuthServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthMonitoring.BLL.Dtos.AccountUserDtos;
+using HealthMonitoring.BLL.Dtos.ApplicationUserDtos;
+
+namespace HealthMonitoring.BLL.IServices
+{
+    public interface IAuthServices
+    {
+        public Task<bool> IsUniqueUser(string username);
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+        public Task<ApplicationUserReadDto> Register(RegisterRequestDto registerRequestDto);
+        public Task UpdateUser(ApplicationUserUpdateDto userUpdateDto,string id);
+    }
+}
