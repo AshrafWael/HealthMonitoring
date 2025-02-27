02f208e7df4d0b8a28bbf18e49024749d97c0e55
Ashraf Wael <ashrafwael582@gmail.com>
2025-02-27 13:30:03 +0200

    add mapping from dtos to models

---
+++ HealthMonitoring.BLL/AutoMapper/MppingProfille.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HealthMonitoring.BLL.Dtos.AccountUserDtos;
+using HealthMonitoring.BLL.Dtos.ActivityDataDtos;
+using HealthMonitoring.BLL.Dtos.ApplicationUserDtos;
+using HealthMonitoring.DAL.Data.Models;
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace HealthMonitoring.BLL.AutoMapper
+{
+    public class MppingProfille : Profile
+    {
+        public MppingProfille()
+        {
+     //       CreateMap<ActivityData, ActivityDataReadDto>()
+     //.ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId));
+
+
+            CreateMap<ActivityData, ActivityDataCreateDto>().ReverseMap();
+            CreateMap<ActivityData, ActivityDataReadDto>().ReverseMap();
+            CreateMap<ActivityData, ActivityDataUpdateDto>().ReverseMap();
+
+            CreateMap<ApplicationUser, ApplicationUserReadDto>().ReverseMap();
+            CreateMap<ApplicationUser, ApplicationUserUpdateDto>().ReverseMap();
+
+            CreateMap<ApplicationUser, LoginRequest>().ReverseMap();
+            CreateMap<ApplicationUser, RegisterRequestDto>().ReverseMap();
+            CreateMap<ApplicationUser, LoginResponseDto>().ReverseMap();
+
+
+
+
+        }
+    }
+}
