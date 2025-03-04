bc1e622903bbd580a9875637689f6bf7defb492b
Ashraf Wael <ashrafwael582@gmail.com>
2025-03-04 02:18:12 +0200

    feat :Add Api requst class for commincate with External Api

---
+++ HealthMonitoring.BLL/APIRequst/APIRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using static HealthMonitoring.BLL.StaticData.StaticData;
+
+namespace HealthMonitoring.BLL.APIRequst
+{
+    public class APIRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string ApiUrl { get; set; }
+        public object Data { get; set; }
+    }
+}
