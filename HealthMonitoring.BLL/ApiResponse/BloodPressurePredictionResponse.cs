a639a9943c7e8830f93e55d8edc434e7df757dbd
Ashraf Wael <ashrafwael582@gmail.com>
2025-03-24 03:24:12 +0200

    responce for predection from ai model

---
+++ HealthMonitoring.BLL/ApiResponse/BloodPressurePredictionResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthMonitoring.API.ApiResponse;
+using HealthMonitoring.DAL.Data.Models.AIModels;
+
+namespace HealthMonitoring.BLL.ApiResponse
+{
+    public class BloodPressurePredictionResponse :APIResponse
+    {
+        public BloodPressurePrediction Prediction { get; set; }
+
+    }
+}
