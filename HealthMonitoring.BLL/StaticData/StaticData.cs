8d28ab09073e05d561e96357b1615f7591c9974c
Ashraf Wael <ashrafwael582@gmail.com>
2025-03-25 01:22:28 +0200

    add api types

---
+++ HealthMonitoring.BLL/StaticData/StaticData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthMonitoring.BLL.StaticData
+{
+    public static class StaticData
+    {
+        //API Type
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+
+    }
+}
