094bc335f7f773db2ee961ef0bc11a2e93962b02
Ashraf Wael <ashrafwael582@gmail.com>
2025-03-25 01:20:58 +0200

    add static data for blood pressure prediction

---
+++ HealthMonitoring.DAL/Consts/StaticData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthMonitoring.DAL.Consts
+{
+    public static  class StaticData
+    {
+        // Blood pressure categories
+        public enum BloodPressureCategory
+        {
+            Low,
+            Normal,
+            Elevated,
+            HypertensionStage1,
+            HypertensionStage2,
+            HypertensiveCrisis
+        }
+    }
+}
+
