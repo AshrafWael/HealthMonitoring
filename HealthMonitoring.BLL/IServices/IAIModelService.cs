d5b6948865f7ff51074927bf7c06e02eee9db0de
Ashraf Wael <ashrafwael582@gmail.com>
2025-03-26 01:14:41 +0200

    feat :add function to predict blood pressure

---
+++ HealthMonitoring.BLL/IServices/IAIModelService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthMonitoring.DAL.Data.Models.AIModels;
+
+namespace HealthMonitoring.BLL.IServices
+{
+    public interface IAIModelService
+    {
+        public  Task<BloodPressurePrediction> PredictBloodPressure(string userid);
+
+    }
+}
