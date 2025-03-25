ee2410b926f8c49ed2560b0476c239af240585e6
Ashraf Wael <ashrafwael582@gmail.com>
2025-03-26 01:16:19 +0200

    sensor data service to connect with ai model

---
+++ HealthMonitoring.BLL/IServices/ISensorDataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthMonitoring.BLL.Dtos.AIModelDtos;
+using HealthMonitoring.DAL.Data.Models.AIModels;
+using static HealthMonitoring.DAL.Consts.StaticData;
+
+namespace HealthMonitoring.BLL.IServices
+{
+    public interface ISensorDataService
+    {
+       
+
+        Task<int> ImportBulkSensorDataAsync(DataPointDto data, string userId);
+        Task<int> ImportBulkSensorDataFromJsonFileAsync(string jsonFilePath, string userId);
+        Task<bool> CacheDatasetAsync(DataPointDto data, string cacheKey);
+        Task<DataPointDto> GetCachedDatasetAsync(string cacheKey);
+
+        Task<List<DataPointDto>> GetDataSetByUser( string userId);
+        public  Task<List<DataPointDto>> GetDataSetByUserChunked(string userId, int chunkSize = 250);
+
+    }
+}
