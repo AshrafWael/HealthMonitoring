5837a90c9a2887c39f96716c3db9b415934a63d1
Ashraf Wael <ashrafwael582@gmail.com>
2025-03-26 01:15:49 +0200

    ai model service for local data

---
+++ HealthMonitoring.BLL/IServices/ILocalAIModelService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthMonitoring.DAL.Data.Models.AIModels;
+
+namespace HealthMonitoring.BLL.IServices
+{
+    public interface ILocalAIModelService
+    {
+        public  Task<BloodPressurePrediction> PredictBloodPressure(List<float> ecgValues, List<float> ppgValues);
+    }
+}
