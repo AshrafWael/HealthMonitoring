40d544fccf287289e0dd41c0d710ccd0187c821b
Ashraf Wael <ashrafwael582@gmail.com>
2025-03-26 01:11:54 +0200

    faet:add new two function for blood pressure predection

---
+++ HealthMonitoring.DAL/IRepository/IAIRepository/IBloodPressureReadingRepository .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthMonitoring.DAL.Data.Models.AIModels;
+
+namespace HealthMonitoring.DAL.IRepository.IAIRepository
+{
+    public interface IBloodPressureReadingRepository :IBaseRepository<BloodPressureReading>
+    {
+        Task<List<BloodPressureReading>> GetRecentByUserIdAsync(string userId, int count = 10);
+        Task<BloodPressureReading> GetLatestByUserIdAsync(string userId);
+
+    }
+}
