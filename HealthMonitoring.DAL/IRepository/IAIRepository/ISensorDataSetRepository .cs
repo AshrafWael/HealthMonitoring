d75c5c8e1eb6932ce277d2f2b9ed628a457c18fe
Ashraf Wael <ashrafwael582@gmail.com>
2025-03-26 01:12:34 +0200

    feat :add three function to get dta user

---
+++ HealthMonitoring.DAL/IRepository/IAIRepository/ISensorDataSetRepository .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthMonitoring.DAL.Data.Models.AIModels;
+
+namespace HealthMonitoring.DAL.IRepository.IAIRepository
+{
+    public interface ISensorDataSetRepository :IBaseRepository<SensorDataPoint>
+    {
+        Task<List<SensorDataPoint>> GetByUserIdAsync(string userId,int count, int skip = 0);
+        public  Task<List<SensorDataPoint>> GetByUserIdBatchedAsync(string userId, int batchSize, int totalNeeded);
+        public Task Addrange(List<SensorDataPoint> batch);
+
+    }
+}
