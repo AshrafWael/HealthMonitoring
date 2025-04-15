dbe0790b701480f1f7cf4aae436baec69a9b2c13
Ashraf Wael <ashrafwael582@gmail.com>
2025-04-15 15:09:15 +0200

    feat: Get By id return list

--- HealthMonitoring.DAL/IRepository/IAIRepository/ISensorDataSetRepository .cs
+++ HealthMonitoring.DAL/IRepository/IAIRepository/ISensorDataSetRepository .cs
@@ -9,7 +9,7 @@ namespace HealthMonitoring.DAL.IRepository.IAIRepository
 {
     public interface ISensorDataSetRepository :IBaseRepository<SensorDataSet>
     {
-         Task<List<SensorDataSet>> GetByUserIdAsync(string userId,int count, int skip = 0);
+         Task<List<SensorDataSet>> GetByUserIdAsync(string userId,int count, int skip =0);
          Task<List<SensorDataSet>> GetByUserIdBatchedAsync(string userId, int batchSize, int totalNeeded);
         Task AddrangeAsync(List<SensorDataSet> sensorDatas);
 
