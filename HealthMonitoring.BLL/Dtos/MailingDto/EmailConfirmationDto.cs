b08335a5970dc18392de08ee910888a186f619d3
Ashraf Wael <ashrafwael582@gmail.com>
2025-04-15 15:14:22 +0200

    Email Confirmation Dto For Registration

---
+++ HealthMonitoring.BLL/Dtos/MailingDto/EmailConfirmationDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthMonitoring.BLL.Dtos.MailingDto
+{
+    public class EmailConfirmationDto
+    {
+        [Display(Name = "Email Address")]
+        public string Email { get; set; } = null!;
+
+
+        [Display(Name = "Verification Code")]
+        public string VerificationCode { get; set; } = null!;
+    }
+}
