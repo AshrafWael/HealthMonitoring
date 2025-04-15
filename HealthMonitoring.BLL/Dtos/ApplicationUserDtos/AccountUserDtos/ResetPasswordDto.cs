e2ae7fbf69a8fd2d60e743147b445d69e3de4147
Ashraf Wael <ashrafwael582@gmail.com>
2025-04-15 15:15:23 +0200

    Update Dtos

---
+++ HealthMonitoring.BLL/Dtos/ApplicationUserDtos/AccountUserDtos/ResetPasswordDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthMonitoring.BLL.Dtos.ApplicationUserDtos.AccountUserDtos
+{
+    public class ResetPasswordDto
+    {
+        [Required]
+        public string email { get; set; } = null!;
+
+        public string CurrentPassword { get; set; } = null!;
+
+        public string NewPassword { get; set; } = null!;
+    }
+}
