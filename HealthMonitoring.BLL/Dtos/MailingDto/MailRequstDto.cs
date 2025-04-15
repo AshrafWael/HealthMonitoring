07b90169cc534b524d90742640e8ef82b64fc87c
Ashraf Wael <ashrafwael582@gmail.com>
2025-04-15 15:13:39 +0200

    Email Request Dto

---
+++ HealthMonitoring.BLL/Dtos/MailingDto/MailRequstDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace HealthMonitoring.BLL.Dtos.MailingDto
+{
+    public class MailRequstDto
+    {
+        [Required]
+        public string ToEmail { get; set; }
+        [Required]
+
+        public string Subject { get; set; }
+        [Required]
+
+        public string Body { get; set; }
+        public  IList<IFormFile> attachments { get; set; }
+
+    }
+}
