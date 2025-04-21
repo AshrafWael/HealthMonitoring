1249af677a73d93b00857a84d6cba9855dc93a0e
Ashraf Wael <ashrafwael582@gmail.com>
2025-04-21 14:49:50 +0200

    add ECG Reading class to get from database

---
+++ HealthMonitoring.DAL/Data/Models/AIModels/ECGReading.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthMonitoring.DAL.Data.Models.AIModels
+{
+    public class ECGReading
+    {
+        public string UserId { get; set; }
+        public double ECG { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
