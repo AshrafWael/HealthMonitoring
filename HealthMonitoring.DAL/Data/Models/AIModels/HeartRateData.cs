3b9fee7a0c5f89aa6244ac84b20aad4c9c56c01a
Ashraf Wael <ashrafwael582@gmail.com>
2025-04-21 14:50:05 +0200

    updated

---
+++ HealthMonitoring.DAL/Data/Models/AIModels/HeartRateData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HealthMonitoring.DAL.Consts.StaticData;
+
+namespace HealthMonitoring.DAL.Data.Models.AIModels
+{
+    public class HeartRateData
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime RecordedAt { get; set; }
+        public int HeartRate { get; set; }
+        public HeartRateCategory Category { get; set; } 
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+    }
+}
