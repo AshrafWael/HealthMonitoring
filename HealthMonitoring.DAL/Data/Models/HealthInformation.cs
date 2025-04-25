720ea1d0497d96df2693e88b3f63f1c915d93b62
Ashraf Wael <ashrafwael582@gmail.com>
2025-04-25 18:17:31 +0300

    update class remove two proprty

--- HealthMonitoring.DAL/Data/Models/HealthInformation.cs
+++ HealthMonitoring.DAL/Data/Models/HealthInformation.cs
@@ -15,9 +15,8 @@ namespace HealthMonitoring.DAL.Data.Models
         [Key]
         public int Id { get; set; }
         public string Diseases { get; set; } // Comma-separated list of diseases
-        public string ChronicDiseases { get; set; } // Comma-separated list of chronic diseases
-        public string Medications { get; set; } // Comma-separated list of medications
-        public string Allergies { get; set; } // Comma-separated list of allergies
+        public DateTime RecordedAt { get; set; }
+
         [ForeignKey("User")]
         public string UserId { get; set; }
         public ApplicationUser User { get; set; }
