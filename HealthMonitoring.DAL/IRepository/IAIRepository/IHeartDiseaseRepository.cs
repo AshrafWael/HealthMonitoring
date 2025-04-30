2792d7cc7d9256b73eaccc533dce8fe4546dec55
Ashraf Wael <ashrafwael582@gmail.com>
2025-04-30 19:44:21 +0300

    Add HeartDisease Repository

---
+++ HealthMonitoring.DAL/IRepository/IAIRepository/IHeartDiseaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthMonitoring.DAL.Data.Models;
+using HealthMonitoring.DAL.Data.Models.AIModels;
+
+namespace HealthMonitoring.DAL.IRepository.IAIRepository
+{
+    public interface IHeartDiseaseRepository : IBaseRepository<HeartDisease>
+    {
+        Task<IEnumerable<HeartDisease>> GetUserHeartRateDataAsync(string userId);
+        Task<HeartDisease> GetLatestHeartRateAsync(string userId);
+        Task<IEnumerable<HeartDisease>> GetHeartRatesByDateRangeAsync(string userId, DateTime startDate, DateTime endDate);
+    }
+}
---
+++ HealthMonitoring.DAL/Repository/HeartDiseaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthMonitoring.DAL.Data.DbHelper;
+using HealthMonitoring.DAL.Data.Models;
+using HealthMonitoring.DAL.Data.Models.AIModels;
+using HealthMonitoring.DAL.IRepository.IAIRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthMonitoring.DAL.Repository
+{
+    public class HeartDiseaseRepository :BaseRepository<HeartDisease> ,IHeartDiseaseRepository
+    {
+        public HeartDiseaseRepository(HealthMonitoringContext dbcontext) : base(dbcontext)
+        {}
+             public async Task<IEnumerable<HeartDisease>> GetUserHeartRateDataAsync(string userId)
+        { 
+
+            IQueryable<HeartDisease> query = _dbset;
+
+            return await query.Where(h => h.UserId == userId)
+                   .OrderByDescending(h => h.RecordedAt)
+                   .ToListAsync();
+        }
+
+        public async Task<HeartDisease> GetLatestHeartRateAsync(string userId)
+        {
+            IQueryable<HeartDisease> query = _dbset;
+            return await query.Where(h => h.UserId == userId)
+                .OrderByDescending(h => h.RecordedAt)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<HeartDisease>> GetHeartRatesByDateRangeAsync(string userId, DateTime startDate, DateTime endDate)
+        {
+            IQueryable<HeartDisease> query = _dbset;
+
+            return await query.Where(h => h.UserId == userId &&
+                           h.RecordedAt >= startDate &&
+                           h.RecordedAt <= endDate)
+                            .OrderBy(h => h.RecordedAt)
+                            .ToListAsync();
+        }
+
+    }
+    }
+
