26bf2318cd9a4606785d0f16763280c149240d06
Ashraf Wael <ashrafwael582@gmail.com>
2025-05-01 21:03:17 +0300

    Blood pressure reed dto

---
+++ HealthMonitoring.BLL/Dtos/BloodPressureDto/BloodPressurReadDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthMonitoring.BLL.Dtos.BloodPressureDto
+{
+    public class BloodPressurReadDto
+    {
+        public DateTime Timestamp { get; set; }
+        public double sbp { get; set; }
+        public double dbp { get; set; }
+        public string Category { get; set; }
+    }
+}
