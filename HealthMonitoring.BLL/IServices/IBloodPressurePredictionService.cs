79d140440727b26878c6c5592d9231587ae70e4a
Ashraf Wael <ashrafwael582@gmail.com>
2025-05-05 02:16:19 +0300

    Add Detrmaine bloodpressure function and Store blood pressure function

--- HealthMonitoring.BLL/IServices/IBloodPressurePredictionService.cs
+++ HealthMonitoring.BLL/IServices/IBloodPressurePredictionService.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HealthMonitoring.BLL.Dtos.BloodPressureDto;
 using HealthMonitoring.DAL.Data.Models.AIModels;
+using static HealthMonitoring.DAL.Consts.StaticData;
 
 namespace HealthMonitoring.BLL.IServices
 {
     public interface IBloodPressurePredictionService
     {
-        public  Task<BloodPressurePrediction> PredictBloodPressure(string userid);
+        public Task<BloodPressurePrediction> PredictBloodPressure(string userid,int batchNumber);
+        public  BloodPressureCategory DetermineBloodPressureCategory(double systolic, double diastolic);
+        public Task StoreBloodPressurePrediction(string userId, double systolic, double diastolic, BloodPressureCategory category);
+        public Task<List<BloodPressurReadDto>> GetRecentReadingsAsync(string userId );
+        public Task<BloodPressurReadDto> GetLatestReadingAsync(string userId);
+        public Task<IEnumerable<BloodPressurReadDto>> GetReadingsByDateRangeAsync(string userId, DateTime start, DateTime end);
 
     }
 }
--- HealthMonitoring.BLL/Services/BloodPressurePredictionService.cs
+++ HealthMonitoring.BLL/Services/BloodPressurePredictionService.cs
@@ -6,16 +6,21 @@ using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using HealthMonitoring.BLL.APIRequst;
 using HealthMonitoring.BLL.ApiResponse;
 using HealthMonitoring.BLL.Dtos.AIModelDtos;
+using HealthMonitoring.BLL.Dtos.BloodPressureDto;
 using HealthMonitoring.BLL.IServices;
 using HealthMonitoring.DAL.Data.Models.AIModels;
+using HealthMonitoring.DAL.UnitOfWork;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 using static HealthMonitoring.BLL.StaticData.StaticData;
+using static HealthMonitoring.DAL.Consts.StaticData;
 
 namespace HealthMonitoring.BLL.Services
 {
@@ -24,23 +29,28 @@ namespace HealthMonitoring.BLL.Services
         private readonly HttpClient _httpClient;
         private readonly ISensorDataService _sensorDataService;
         private readonly ILogger _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
         private readonly string _serviceURL;
 
         public BloodPressurePredictionService(HttpClient httpClient, IConfiguration configuration
-            ,ISensorDataService sensorDataService , ILogger<BloodPressurePredictionService> logger) 
+            , ISensorDataService sensorDataService, ILogger<BloodPressurePredictionService> logger
+            , IUnitOfWork unitOfWork,IMapper mapper)
         {
             _httpClient = httpClient;
             _sensorDataService = sensorDataService;
             _logger = logger;
-            _serviceURL = configuration.GetValue<string>("AIModel:BaseUrl")!;
-
-
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _serviceURL = configuration.GetValue<string>("AIModel:BloodPressurUrl")!;
         }
-
-        public async Task<BloodPressurePrediction> PredictBloodPressure(string userId)
+        public async Task<BloodPressurePrediction> PredictBloodPressure(string userId,int batchNumber)
         {
             // Fetch latest 1250 sensor data records
-            var latestData = await _sensorDataService.GetDataSetByUser(userId);
+            
+                var latestData = await _sensorDataService.GetDataSetByUser(userId , batchNumber);
+                 
+
             if (!latestData.Any())
             {
                 throw new Exception("No sensor data found for the user.");
@@ -110,5 +120,65 @@ namespace HealthMonitoring.BLL.Services
         //    return response;
 
         //}
+        public BloodPressureCategory DetermineBloodPressureCategory(double systolic, double diastolic)
+        {
+            if (systolic < 120 && diastolic < 80)
+                return BloodPressureCategory.Normal;
+            else if ((systolic >= 120 && systolic <= 129) && diastolic < 80)
+                return BloodPressureCategory.Elevated;
+            else if ((systolic >= 130 && systolic <= 139) || (diastolic >= 80 && diastolic <= 89))
+                return BloodPressureCategory.HypertensionStage1;
+            else if (systolic >= 140 || diastolic >= 90)
+                return BloodPressureCategory.HypertensionStage2;
+            else if (systolic > 180 || diastolic > 120)
+                return BloodPressureCategory.HypertensiveCrisis;
+            else
+                return BloodPressureCategory.HypertensiveCrisis;
+        }
+
+        public async Task StoreBloodPressurePrediction(string userId, double systolic, double diastolic, BloodPressureCategory category)
+        {
+            try
+            {
+                var reading = new BloodPressureReading
+                {
+                    UserId = userId,
+                    Timestamp = DateTime.UtcNow,
+                    sbp = systolic,
+                    dbp = diastolic,
+                    Category = category
+                };
+
+                await _unitOfWork.bloodPressureReading.CreateAsync(reading);
+                _unitOfWork.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to store blood pressure prediction");
+                // Don't throw - we want to return the prediction even if storage fails
+            }
+        }
+
+        public async Task<List<BloodPressurReadDto>> GetRecentReadingsAsync(string userId )
+        {
+
+           var data = await _unitOfWork.bloodPressureReading.GetRecentBloodPressureByUserIdAsync(userId);
+            var mappeddata = _mapper.Map<List<BloodPressurReadDto>>(data);
+            return mappeddata;
+        }
+        public async Task<BloodPressurReadDto> GetLatestReadingAsync(string userId)
+        {
+         var data=  await _unitOfWork.bloodPressureReading.GetLatestBloodPressureByUserIdAsync(userId);
+            var mappeddata = _mapper.Map<BloodPressurReadDto>(data);
+            return mappeddata;
+        }
+        
+        public async Task<IEnumerable<BloodPressurReadDto>> GetReadingsByDateRangeAsync(string userId, DateTime start, DateTime end)
+        {
+          var data =  await _unitOfWork.bloodPressureReading.GetBloodPressureByDateRangeAsync(userId, start, end);
+            var mappeddata = _mapper.Map<IEnumerable<BloodPressurReadDto>>(data);
+            return mappeddata;
+        }
+
     }
 }
